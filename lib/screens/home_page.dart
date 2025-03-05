c2673375739aa9da06d4acf6090c0d5bee372770
Loujayn234 <lojy.dohan@gmail.com>
2025-03-05 02:00:42 +0200

    Chatbot + Notifications

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:car_maintenance/AI-Chatbot/chatbot.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,26 +14,41 @@ class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
   final CollectionReference users =
       FirebaseFirestore.instance.collection('users');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.white,
-      appBar:
-          AppBar(centerTitle: true, title: const Text('Home Page'), actions: [
-        IconButton(
-          icon: const Icon(Icons.logout),
-          onPressed: () {
-            FirebaseAuth.instance.signOut();
-          },
-        )
-      ]),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Home Page'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          )
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
           children: [
-            Text("signed in as ${user.email}"),
+            Text("Signed in as ${user.email}"),
           ],
         ),
+      ),
+     floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Navigate to Chatbot screen
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Chatbot()),
+          );
+        },
+        label: const Text('Chatbot'),
+        icon: const Icon(Icons.chat),
+        backgroundColor: Color(0xFFD1A3FF), // Light purple
       ),
     );
   }
