95788bd8e5310c0651d331c79b230aaeea6aa008
hadeerqaem <hadeerqasem3@gmail.com>
2025-03-11 21:02:34 +0200

    Improve Welcome Page UI and use AssetImage

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF2C2B33);
+  static const Color primaryText = Colors.white;
+  static const Color secondaryText = Color(0xFFD9D9D9);
+  static const Color buttonColor = Color(0xFFD9D9D9);
+  static const Color buttonText = Color(0xFF2C2B33);
+}
