6390a01c357e605d4e9f6381a9a6ab2b805c43c7
hadeerqaem <hadeerqasem3@gmail.com>
2025-03-12 18:37:16 +0200

    Improve login Page UI

--- lib/screens/Auth/check_auth_status.dart
+++ lib/screens/Auth/check_auth_status.dart
@@ -1,6 +1,5 @@
 import 'package:car_maintenance/screens/Auth/auth_page.dart';
 import 'package:car_maintenance/screens/Current_Screen/main_screen.dart';
-import 'package:car_maintenance/screens/welcome_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +9,13 @@ class CheckAuthStatus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(), 
-      builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return MainScreen();
-            } else {
-              return AuthPage();
-            }
-            }
-      );
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return MainScreen();
+          } else {
+            return AuthPage();
+          }
+        });
   }
 }
