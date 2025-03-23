160d341163917296e181a171e2b24ec5fee31345
hadeerqaem <hadeerqasem3@gmail.com>
2025-03-23 02:26:16 +0200

    edit new colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color background = Color(0xFF2C2B33);
-  static const Color primaryText = Colors.white;
-  static const Color secondaryText = Color(0xFFD9D9D9);
-  static const Color buttonColor = Color(0xFFD9D9D9);
-  static const Color buttonText = Color(0xFF2C2B33);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color primaryText = Color(0xFF2E2E2E);
+  static const Color secondaryText = Color(0xFFF4F4F4);
+  static const Color buttonColor = Color(0xFFDA1F11);
+  static const Color buttonText = Color(0xFFFFFFFF);
+  static const Color lightGray = Color(0xFFF2F2F2);
+  static const Color borderSide = Color(0xFFD9D9D9);
+
 }
