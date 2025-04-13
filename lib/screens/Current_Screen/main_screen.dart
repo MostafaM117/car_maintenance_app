0bd97afcc82279309d5c4161ce1cf3e2ca9e12cc
Mostafaa117 <mostafa.hamed1173@gmail.com>
2025-04-13 02:03:30 +0200

    merge branch mostafa

--- lib/screens/Current_Screen/main_screen.dart
+++ lib/screens/Current_Screen/main_screen.dart
@@ -19,7 +19,7 @@ class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-     HomePage(),
+    HomePage(),
     const MaintenanceScreen(),
     const Chatbot(),
     const Market(),
