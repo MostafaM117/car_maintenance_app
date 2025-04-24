b764b1fa00791c5f63fd0d5f4610d91eb984a545
hadeerqaem <hadeerqasem3@gmail.com>
2025-04-24 23:07:57 +0200

    seller profile screen ui

--- lib/screens/seller_screens/seller_profile.dart
+++ lib/screens/seller_screens/seller_profile.dart
@@ -10,6 +10,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../widgets/profile_option_tile.dart.dart';
+
 class SellerProfile extends StatefulWidget {
   const SellerProfile({super.key});
 
@@ -50,134 +52,241 @@ class _SellerProfileState extends State<SellerProfile> {
 
   @override
   Widget build(BuildContext context) {
-    // Create button data inside the build method
-    final List<Map<String, dynamic>> buttonData = [
-      {
-        'text': 'Manage your account',
-        'onPressed': () => Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => UserAccountManagement()),
-            ),
-      },
-      // {
-      //   'text': 'My Cars',
-      //   'onPressed': () => Navigator.push(
-      //         context,
-      //         MaterialPageRoute(builder: (context) => CarMaint()),
-      //       ),
-      // },
-      {'text': 'User Guide', 'onPressed': () => ()},
-      {
-        'text': 'Log Out',
-        'onPressed': () {
-          AuthService().signOut(context);
-        },
-      },
-    ];
-
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: SafeArea(
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            // Main container
-            Container(
-              margin: const EdgeInsets.only(top: 100),
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height - 75,
-              decoration: ShapeDecoration(
-                color: AppColors.secondaryText,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(width: 1, color: Color(0xFFE7E7E7)),
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
-                  ),
+      body: Stack(
+        children: [
+          Container(
+            width: 393,
+            height: 250,
+            decoration: ShapeDecoration(
+              color: AppColors.secondaryText,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  width: 1,
+                  color: const Color(0xFFE3E3E3),
                 ),
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Text(
-                        '${seller.email}',
-                        style: TextStyle(
-                          color: Colors.black.withOpacity(0.7),
-                          fontSize: 13,
-                          fontFamily: 'Inter',
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 130),
-                    for (var button in buttonData)
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: buildButton(
-                          button['text'],
-                          button['text'] == 'Log Out'
-                              ? AppColors.buttonColor
-                              : AppColors.secondaryText.withOpacity(0.9),
-                          button['text'] == 'Log Out'
-                              ? AppColors.secondaryText
-                              : Colors.black,
-                          onPressed: button['onPressed'],
-                        ),
-                      ),
-                  ],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
                 ),
               ),
             ),
-
-            Positioned(
-              top: 40,
-              left: 30,
-              right: 00,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: _profileImage != null
-                        ? CircleAvatar(
-                            radius: 40,
-                            backgroundImage: FileImage(_profileImage!),
-                          )
-                        : CircleAvatar(
-                            radius: 40,
-                            backgroundColor: AppColors.secondaryText,
-                            child: Icon(
-                              Icons.person,
-                              color: Colors.white,
-                              size: 50,
-                            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: _profileImage != null
+                      ? CircleAvatar(
+                          radius: 40,
+                          backgroundImage: FileImage(_profileImage!),
+                        )
+                      : CircleAvatar(
+                          radius: 40,
+                          backgroundColor: AppColors.secondaryText,
+                          child: Icon(
+                            Icons.person,
+                            color: Colors.white,
+                            size: 50,
                           ),
+                        ),
+                ),
+                Text(
+                  username,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primaryText,
+                    fontFamily: 'Inter',
                   ),
-                  const SizedBox(width: 15),
-                  Expanded(
-                    child: Text(
-                      username,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.primaryText,
-                        fontFamily: 'Inter',
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
+                ),
+                Text(
+                  '${seller.email}',
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                    fontSize: 13,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w500,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                ProfileOptionTile(
+                  rightIcon: Icons.person,
+                  text: 'Profile',
+                  onBackTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => UserAccountManagement()),
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ProfileOptionTile(
+                  rightIcon: Icons.settings,
+                  text: 'Settings',
+                  onBackTap: () {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ProfileOptionTile(
+                  rightIcon: Icons.history,
+                  text: 'Activity',
+                  onBackTap: () {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ProfileOptionTile(
+                  rightIcon: Icons.security,
+                  text: 'Security & Privacy',
+                  onBackTap: () {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ProfileOptionTile(
+                  rightIcon: Icons.help_outline,
+                  text: 'Help & Support',
+                  onBackTap: () {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: buildButton(
+                    'Log Out',
+                    AppColors.buttonColor,
+                    AppColors.buttonText,
+                    onPressed: () {
+                      AuthService().signOut(context);
+                    },
+                  ),
+                ),
+  
+              ],
+            ),
+          ),
+        ],
       ),
+      // child: Stack(
+      //   alignment: Alignment.topCenter,
+      //   children: [
+      //     // Main container
+      //     Container(
+      //       margin: const EdgeInsets.only(top: 100),
+      //       width: double.infinity,
+      //       height: MediaQuery.of(context).size.height - 75,
+      //       decoration: ShapeDecoration(
+      //         color: AppColors.secondaryText,
+      //         shape: RoundedRectangleBorder(
+      //           side: const BorderSide(width: 1, color: Color(0xFFE7E7E7)),
+      //           borderRadius: const BorderRadius.only(
+      //             topLeft: Radius.circular(50),
+      //             topRight: Radius.circular(50),
+      //           ),
+      //         ),
+      //       ),
+      //       child: Padding(
+      //         padding:
+      //             const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
+      //         child: Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           children: [
+      //             Align(
+      //               alignment: Alignment.topRight,
+      //               child:
+      //             ),
+      //             SizedBox(height: 130),
+
+      //             ProfileOptionTile(
+      //               rightIcon: Icons.settings,
+      //               text: 'Settings',
+      //               onBackTap: () {},
+      //             ),
+
+      //             // for (var button in buttonData)
+      //             //   Padding(
+      //             //     padding: const EdgeInsets.only(bottom: 20),
+      //             //     child: buildButton(
+      //             //       button['text'],
+      //             //       button['text'] == 'Log Out'
+      //             //           ? AppColors.buttonColor
+      //             //           : AppColors.secondaryText.withOpacity(0.9),
+      //             //       button['text'] == 'Log Out'
+      //             //           ? AppColors.secondaryText
+      //             //           : Colors.black,
+      //             //       onPressed: button['onPressed'],
+      //             //     ),
+      //             //   ),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+
+      //     Positioned(
+      //       top: 40,
+      //       left: 30,
+      //       right: 00,
+      //       child: Row(
+      //         crossAxisAlignment: CrossAxisAlignment.center,
+      //         children: [
+      //           Container(
+      //             width: 100,
+      //             height: 100,
+      //             child: _profileImage != null
+      //                 ? CircleAvatar(
+      //                     radius: 40,
+      //                     backgroundImage: FileImage(_profileImage!),
+      //                   )
+      //                 : CircleAvatar(
+      //                     radius: 40,
+      //                     backgroundColor: AppColors.secondaryText,
+      //                     child: Icon(
+      //                       Icons.person,
+      //                       color: Colors.white,
+      //                       size: 50,
+      //                     ),
+      //                   ),
+      //           ),
+      //           const SizedBox(width: 15),
+      //           Expanded(
+      //             child: Text(
+      //               username,
+      //               style: const TextStyle(
+      //                 fontSize: 20,
+      //                 fontWeight: FontWeight.bold,
+      //                 color: AppColors.primaryText,
+      //                 fontFamily: 'Inter',
+      //               ),
+      //               overflow: TextOverflow.ellipsis,
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
