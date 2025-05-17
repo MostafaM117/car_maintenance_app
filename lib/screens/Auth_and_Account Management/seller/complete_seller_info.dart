9a8cff01c276371522fa3ad067801cbcd4dfb198
Mostafaa117 <mostafa.hamed1173@gmail.com>
2025-05-18 00:34:00 +0300

    seller signup page 1 finished

---
+++ lib/screens/Auth_and_Account Management/seller/complete_seller_info.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CompleteSellerInfo extends StatefulWidget {
+  const CompleteSellerInfo({super.key});
+
+  @override
+  State<CompleteSellerInfo> createState() => _CompleteSellerInfoState();
+}
+
+class _CompleteSellerInfoState extends State<CompleteSellerInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
