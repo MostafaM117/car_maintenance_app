eef8ab8d0392abd7e9e0268254e209d6a4f324a8
hadeerqaem <hadeerqasem3@gmail.com>
2025-05-25 00:36:50 +0300

    m

--- lib/screens/Auth_and_Account Management/seller/complete_seller_info.dart
+++ lib/screens/Auth_and_Account Management/seller/complete_seller_info.dart
@@ -11,8 +11,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
+import 'package:path/path.dart' as path;
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class CompleteSellerInfo extends StatefulWidget {
   final String businessname;
