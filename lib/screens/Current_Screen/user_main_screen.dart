d6ea49f61efb60c955e99968bec9fa77d9a572c2
hadeerqaem <hadeerqasem3@gmail.com>
2025-05-25 02:56:49 +0300

    m

--- lib/screens/Current_Screen/user_main_screen.dart
+++ lib/screens/Current_Screen/user_main_screen.dart
@@ -29,7 +29,7 @@ class _UserMainScreenState extends State<UserMainScreen> {
 
   final List<String> _labels = [
     'Home',
-    'MNT',
+    'Maintain',
     'Chatbot',
     'Market',
     'Profile',
